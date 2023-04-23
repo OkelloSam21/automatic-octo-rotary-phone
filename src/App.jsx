82a74125753bb86094aee70027a8899d8af5d672
OkelloSam21 <osamuel21@student.kibu.ac.ke>
2023-04-23 14:11:18 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Samuel Okello",
+  title: "Software Engineer",
+  email: "samuel.okello@studentambassadors.com",
+  gitHub: "OelloSam21",
+  instagram: "devsam254",
+  linkedIn: "samuel-okello",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "0kello_sam",
+  youTube: "CodingCheos",
 };
 
 const primaryColor = "#4E567E";
