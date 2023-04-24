0357edd02a37a1bda9d26dacc0e03779131865fc
OkelloSam21 <osamuel21@student.kibu.ac.ke>
2023-04-24 22:25:16 +0300

    add education

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@
 import React from "react";
 
 import About from "./Components/About";
+import Education from "./Components/Education";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
@@ -45,6 +46,7 @@ const App = () => {
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
       <Portfolio />
+      <Education />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
