f79fbb3278f81c186692074e418ff410a0f5c053
OkelloSam21 <osamuel21@student.kibu.ac.ke>
2023-04-24 10:11:34 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Samuel Okello",
+  title: "Software Engineer",
+  email: "samuel.okello@studentambassadors.com",
+  gitHub: "OelloSam21",
+  instagram: "devsam254",
+  linkedIn: "samuel-okello",
+  medium: "",
+  twitter: "0kello_sam",
+  youTube: "CodingCheos",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
