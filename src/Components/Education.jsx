ef23e5f61270aa9d91e77bc5b7a1ba977e22ca99
OkelloSam21 <osamuel21@student.kibu.ac.ke>
2023-04-24 23:06:02 +0300

    update education section

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -3,10 +3,28 @@ import React from 'react';
 const Education = () => {
     return(
         <section className ="light" id = "portfolio">
-            <h2>Education</h2>   
+            <h2>Education</h2>
+            <div className="education-container">
+                <div className="education-item">
+                    <h3>Primary School</h3>
+                    <p>Name of School: <strong>[insert name of school]</strong></p>
+                    <p>Date Attended: <strong>[insert start date] - [insert end date]</strong></p>
+                </div>
+                <div className="education-item">
+                    <h3>High School</h3>
+                    <p>Name of School: <strong>[insert name of school]</strong></p>
+                    <p>Date Attended: <strong>[insert start date] - [insert end date]</strong></p>
+                </div>
+                <div className="education-item">
+                    <h3>Higher Learning</h3>
+                    <p>Name of School: <strong>[insert name of school]</strong></p>
+                    <p>Date Attended: <strong>[insert start date] - [insert end date]</strong></p>
+                </div>
+            </div>
         </section>        
     )
 }
 
 export default Education;
+
 
