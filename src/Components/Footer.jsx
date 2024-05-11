48d01c2d48ccfd4e7ef2a0e0d804e250a860dbee
SAMUEL <115361239+OkelloSam21@users.noreply.github.com>
2024-05-11 16:51:16 +0300

    Update Footer.jsx
    removed instagram

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -13,7 +13,6 @@ import PropTypes from "prop-types";
 import devDotToIcon from "../images/socials/devdotto.svg";
 import envelopeIcon from "../images/socials/envelope.svg";
 import gitHubIcon from "../images/socials/github.svg";
-import instagramIcon from "../images/socials/instagram.svg";
 import linkedInIcon from "../images/socials/linkedin.svg";
 import mediumIcon from "../images/socials/medium.svg";
 import twitterIcon from "../images/socials/twitter.svg";
@@ -31,7 +30,6 @@ const Footer = (props) => {
     devDotTo,
     email,
     gitHub,
-    instagram,
     linkedIn,
     medium,
     name,
@@ -72,15 +70,6 @@ const Footer = (props) => {
         {gitHub && (
           <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
-          </a>
-        )}
-        {instagram && (
-          <a
-            href={`https://www.instagram.com/${instagram}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={instagramIcon} alt="Instagram" className="socialIcon" />
           </a>
         )}
         {linkedIn && (
@@ -127,7 +116,6 @@ Footer.propTypes = {
   devDotTo: PropTypes.string,
   email: PropTypes.string,
   gitHub: PropTypes.string,
-  instagram: PropTypes.string,
   linkedIn: PropTypes.string,
   medium: PropTypes.string,
   name: PropTypes.string.isRequired,
